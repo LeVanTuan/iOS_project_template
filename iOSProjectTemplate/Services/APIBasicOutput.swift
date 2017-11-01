9bb93f6ca4662aaada44c7a8a6fe304e90c0fc7f
Tuan LE <TuanLe@Tuans-MacBook-Pro.local>
2017-11-01 15:41:36 +0700

    first commit

---
+++ iOSProjectTemplate/Services/APIBasicOutput.swift
@@ -0,0 +1,9 @@
+//
+//  APIBasicOutput.swift
+//  iOSProjectTemplate
+//
+//  Created by Tuan LE on 11/1/17.
+//  Copyright © 2017 Leo LE. All rights reserved.
+//
+
+import Foundation
---
+++ iOSProjectTemplate/Services/APIUrls.swift
@@ -0,0 +1,9 @@
+//
+//  APIUrls.swift
+//  iOSProjectTemplate
+//
+//  Created by Tuan LE on 11/1/17.
+//  Copyright © 2017 Leo LE. All rights reserved.
+//
+
+import Foundation
